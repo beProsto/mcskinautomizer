ae4fb65c10ed0ad105cbe7718fdad672fc80cb8e
beProsto <lavionperavion2@gmail.com>
2021-07-07 19:02:34 +0200

    stbi

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "ven/stb_image.h"
+#include "ven/stb_image_write.h"
+
+
+int main(int argc, char** argv) {
+
+	uint8_t* img = stbi_load()
+
+	return 0;
+}
